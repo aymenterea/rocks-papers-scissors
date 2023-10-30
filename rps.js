3d1c7d6260e2f61cbbd07ce6ecf4c288f11884eb
aymenterea <aymenterea@gmail.com>
2023-10-30 16:53:27 +0100

    Create the playRound function
    Created the function necessary to decide who won the round.

--- rps.js
+++ rps.js
@@ -1,23 +1,46 @@
 function getComputerChoice() {
-    let pick = Math.random();
+    let num = Math.random();
     let choice;
-    if (pick <= 0.33) {
+    if (num <= 0.33) {
         choice = "Rock"
-    } else if (0.33 < pick && pick <= 0.66) {
+    } else if (0.33 < num && num <= 0.66) {
         choice = "Paper"
-    } else if (pick > 0.66) {
-        choice = "Scissor"
+    } else if (num > 0.66) {
+        choice = "Scissors"
     }   
-
     return choice;
 }
 
-let element;
+let element
 
-function playerSelection() {
+function playerChoice() {
     element = prompt("Please choose either Rock, Paper, or Scissors:")
     if (element.toLowerCase() === "rock" || element.toLowerCase() === "paper" || element.toLowerCase() === "scissors") {
-        element = element.charAt(0).toUpperCase + element.slice(1)
+        element = element[0].toUpperCase() + element.slice(1).toLowerCase();
     } else alert("Sorry, you can only pick one of the suggested options")
+
+    return element;
 }
+
+let playerSelection = playerChoice();
+let ComputerChoice = getComputerChoice();
+
+function playRound(playerSelection, ComputerChoice) {
+    if (playerSelection === "Rock" && ComputerChoice === "Paper") {
+        return "You lose! Paper beats Rock."
+    } else if (playerSelection === "Paper" && ComputerChoice === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (playerSelection === "Paper" && ComputerChoice === "Scissors") {
+        return "You lose! Scissor beats Paper."
+    } else if (playerSelection === "Scissors" && ComputerChoice === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else if (playerSelection === "Rock" && ComputerChoice === "Scissors") {
+        return "You win! Rock beats Scissors."
+    } else if (playerSelection === "Scissors" && ComputerChoice === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else {
+        return `It's a tie! you both used ${playerSelection}`
+    }
+}
+
 
